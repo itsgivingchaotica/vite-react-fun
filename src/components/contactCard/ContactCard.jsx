3ae60829e857b6107499e5e402ca1f6191126a8d
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-06-18 23:45:13 -0400

    install material ui

---
+++ src/components/contactCard/ContactCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './contact_card.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const ContactCard = () => {
+
+    return(
+        <div>Hello</div>
+    )
+}
+
+export default ContactCard;
