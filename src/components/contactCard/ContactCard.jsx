2f1fca0f1e8d79155e93f0e64e13c324bec2cb6b
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-06-18 23:53:49 -0400

    set up contact card component file

--- src/components/contactCard/ContactCard.jsx
+++ src/components/contactCard/ContactCard.jsx
@@ -4,6 +4,12 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const ContactCard = () => {
 
