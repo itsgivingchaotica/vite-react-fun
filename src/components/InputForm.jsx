34360b4f3e871b6faebf760a53038e8e47512171
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-06-19 22:11:33 -0400

    input form functioning with clean ui

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import React, { useState } from 'react'
+import Stack from '@mui/material/Stack'
+import Card from '@mui/material/Card'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import ContactsIcon from '@mui/icons-material/Contacts'
+import AddIcon from '@mui/icons-material/Add'
 
-const InputForm = () => {
+const InputForm = ({addToContacts}) => {
+
+    // CONTACT CARD DATA
+    const [firstName,setFirstName] = useState('');
+    const [lastName,setLastName] = useState('');
+    const [email,setEmail] = useState('');
+    const [mobile, setMobile] = useState('');
+    const [work, setWork] = useState('');
+
+
+    // add the new contact info to the contact cards
+    const handleAddContact = () => {
+        let newContact = { firstName: firstName, lastName: lastName, email: email, mobile: mobile, work: work}
+        addToContacts(newContact);
+    }
 
     return (
-        <div>input form</div>
+        // <div style={{position: 'fixed'}}>
+        <Card sx={{ padding: '36px' }}>
+        <Typography variant="h4" mb={3} sx={{ textAlign: "center"}}><ContactsIcon sx={{mr:1}}/>Contacts</Typography>
+        <Stack spacing = {2} component="form" >
+            {/* FIRST NAME */}
+            <TextField 
+                id="outlined-basic" 
+                helperText="First Name" 
+                variant="outlined" 
+                defaultValue={firstName}
+                onChange={(e) => setFirstName(e.target.value)}/>
+            {/* LAST NAME */}
+            <TextField 
+                id="outlined-basic" 
+                helperText="Last Name" 
+                variant="outlined" 
+                defaultValue={firstName} 
+                onChange={(e) => setLastName(e.target.value)}/>
+            {/* EMAIL */}
+            <TextField 
+                id="outlined-basic" 
+                helperText="Email" 
+                variant="outlined" 
+                defaultValue={firstName} 
+                onChange={(e) => setEmail(e.target.value)}/>
+            {/* MOBILE */}
+            <TextField 
+                id="outlined-basic" 
+                helperText="Mobile Phone" 
+                variant="outlined" 
+                defaultValue={firstName} 
+                onChange={(e) => setMobile(e.target.value)}/>
+            {/* WORK */}
+            <TextField 
+                id="outlined-basic" 
+                helperText="Work Phone" 
+                variant="outlined" 
+                defaultValue={firstName} 
+                onChange={(e) => setWork(e.target.value)}/>
+            <Button 
+                variant="outlined" 
+                type="submit"  
+                endIcon={<AddIcon />} 
+                onClick={handleAddContact}>
+                Add 
+            </Button>
+        </Stack>
+    </Card>
     )
 }
 
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -5,36 +5,44 @@ import Grid from '@mui/material/Grid'
 export default function Contact(){
     const [contacts,setContacts] = useState([
        {
-            fullName: 'John Smith',
+            firstName: 'John',
+            lastName: 'Smith',
             email: 'johnsmith@me.com',
             mobile: '555-123-4567',
             work: '555-987-6543',
 
         },
         {
-            fullName: 'Emily Johnson',
+            firstName:'Emily',
+            lastName: 'Johnson',
             email: 'emilyjohnson@gmail.com',
             mobile: '555-234-5678',
             work: '555-623-0593',
         },
         {
-            fullName: 'Michael Davis',
+            firstName: 'Michael',
+            lastName: 'Davis',
             email: 'michaeldavis@hotmail.com',
             mobile: '555-345-6789',
             work: '555-765-4321'
         },
         {
-            fullName: 'Sophia Wilson',
+            firstName: 'Sophia',
+            lastName: 'Wilson',
             email: 'sophiawilson@icloud.com',
             mobile: '555-456-7890',
         }  
     
     ]);
 
+    const addToContacts = (newContact) => {
+        setContacts((contacts) => [...contacts, newContact]);
+    }
+
      return (
      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} justifyContent="flex-start">
       <Grid item xs={12} md={4}>
-        <com.InputForm />
+        <com.InputForm addToContacts={addToContacts}/>
       </Grid>
       <Grid item xs={12} md={8}>
         <Grid container spacing={2} >
